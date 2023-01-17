0ccb4349a3c35f2ee7d63fa6b3c3016e88c8ffad
Zhi <neozhixuan@gmail.com>
2023-01-17 10:11:24 +0800

    Button disabler

--- pages/components/mainapppage.js
+++ pages/components/mainapppage.js
@@ -166,7 +166,7 @@ const MainAppPage = (props) => {
       </div>
       <form className="w-[100%] md:w-[50%]">
         {" "}
-        <div className="flex flex-col mx-[25%] md:mr-[25%]">
+        <div className="flex flex-col ml-[25%] md:ml-[0%] mr-[25%]">
           <label
             for="first_name"
             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -183,7 +183,7 @@ const MainAppPage = (props) => {
             value={area}
           />
         </div>
-        <div className="flex flex-col  mx-[25%] md:mr-[25%] mt-5">
+        <div className="flex flex-col ml-[25%] md:ml-[0%] mr-[25%] mt-5">
           <label
             for="message"
             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -202,6 +202,7 @@ const MainAppPage = (props) => {
         <div className="md:mx-0 mx-[25%] mt-5">
           {" "}
           <button
+            disabled={area == "" ? true : false}
             onClick={submitHandler}
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           >
